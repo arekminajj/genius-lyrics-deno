930cfdf08549146fa1eb8f566d41462c7472016b
Arek Cios <arek123113@gmail.com>
2020-08-20 11:06:16 +0200

    Add test

---
+++ test/searchSong.test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { searchSong } from '../src/searchSong.ts'
+
+Deno.test("searchSongTest", async () => {
+    const params = {
+        q: "awful things",
+        access_token:
+          "26eksgdkCd6cjQ1lXGy__1_lJIbh2_OXyT70U-aCzsH-5goq5GlU2SAlv25kt1af",
+      };
+    const result = await searchSong(params);
+    assertEquals(await result.title, "Awful Things");
+  });
