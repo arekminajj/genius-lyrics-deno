bae0fa7516f14b2004e5c7449caeecb97ced565b
Arek Cios <arek123113@gmail.com>
2020-08-23 16:52:19 +0200

    Add test for getArtistId

---
+++ test/getArtistId.test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "../deps.ts";
+import { getArtistId } from '../src/getArtistId.ts'
+
+Deno.test("getArtistIdTest", async () => {
+    const params = {
+        q: "lil tracy",
+        access_token:
+          "26eksgdkCd6cjQ1lXGy__1_lJIbh2_OXyT70U-aCzsH-5goq5GlU2SAlv25kt1af",
+      };
+    const result = await getArtistId(params);
+    assertEquals(await result, 1015803);
+  });
